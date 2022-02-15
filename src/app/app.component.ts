39086ab61f8dcfa6c6379a5bd8d35ce100757e15
Ryan Mc Donald <S00199690@mail.itsligo.ie>
2022-02-15 21:52:11 +0000

    book app complete

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FirebaseApiService } from './firebase-api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  MyBooks: any = [];
+  titleValue = '';
+  authorValue = '';
+
+  constructor(public firebaseApiService: FirebaseApiService) {
+
+  }
+
+  ngOnInit() {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    return this.firebaseApiService.getBooks().subscribe((data: {}) => {
+      this.MyBooks = data;
+    })
+  }
+
+  addBook() {
+    return this.firebaseApiService.addBook(this.titleValue, this.authorValue).subscribe((data: {}) => {
+      this.MyBooks = data;
+      this.titleValue = '';
+      this.authorValue = '';
+    })
+  }
+
+  deleteBook(id: string) {
+    return this.firebaseApiService.delBook(id).subscribe((data: {}) => {
+      this.MyBooks = data;
+    })
+  }
+}
